688f50862c4ed583e1d2f0b12aa0be54c2991932
Bibek Gauli <bibekgauli11@gmail.com>
2026-06-16 22:27:59 +0545

    symbol in amount updated

--- src/app/refunds/components/RefundTable.tsx
+++ src/app/refunds/components/RefundTable.tsx
@@ -162,7 +162,7 @@ export function RefundTable({
         meta: { sortKey: "amount" },
         cell: ({ row }) => (
           <span className="px-2 py-1 text-xs font-medium rounded-full">
-            {formatCurrency(row.original.amount, row.original.symbol)}
+            {formatCurrency(row.original.amount, row.original.event.symbol)}
           </span>
         ),
       },
